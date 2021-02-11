ef5ce40b25dd682c0cd73a0fe1f9b2476292d7ad
Sabrina Lewis <sabrina.lewis@bryanuniversity.edu>
2021-02-11 13:19:40 -0600

    changed api call to https

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function getWeatherData(e) {
     let city = searchParams.value
 
     // forecast URL
-    const forecastURL = `http://api.weatherapi.com/v1/forecast.json?key=83d976727511405bad3224359211002&q=${city}&days=3`
+    const forecastURL = `https://api.weatherapi.com/v1/forecast.json?key=83d976727511405bad3224359211002&q=${city}&days=3`
 
 
     axios.get(forecastURL)
